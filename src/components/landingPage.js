38a5ae35339e9850a006b430b4f0b48e190874ff
Md Akram Kazmi <akramkazmi71@gmail.com>
2020-10-01 16:44:00 +0530

    adding blank files

---
+++ src/components/landingPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './landingPage.css';
+
+class Landing extends Component {
+    render() {
+        return(
+            <div>
+                Hello
+            </div>
+        )
+    }
+}
+
+export default Landing;
+
---
+++ src/components/main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './main.css';
+import Landing from './landingPage.js';
+
+
+const Main = () => (
+    <Landing />
+)
+
+export default Main;
