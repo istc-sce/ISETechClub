f1f83a266950c523db515378eb307be72c3467e4
niranjan hegde <hegdeniranjan144@gmail.com>
2020-10-02 12:33:01 +0530

    Added contributors.js

---
+++ src/components/Competetive.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Competetive() {
+  return (
+    <div>
+      <form action="#" method="POST">
+        <input type="file" name="solution" id="solution" />
+      </form>
+    </div>
+  );
+}
---
+++ src/components/Contributors.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+let mapDetail = (detail) => {
+  console.log(detail);
+  return (
+    <div className="contributor" key={detail.id}>
+      <img src={detail.profile} alt="profile" />
+      <div className="about">
+        <div className="name">{detail.name}</div>
+        <div className="desc">{detail.description}</div>
+      </div>
+      <div className="contact">
+        {detail.contact.map((contact) => (
+          <a key="contact.id" href={contact.link}>
+            Profile
+          </a>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default function Contributors(props) {
+  const data = props.details;
+  return (
+    <div className="profile">{data.map((detail) => mapDetail(detail))}</div>
+  );
+}
