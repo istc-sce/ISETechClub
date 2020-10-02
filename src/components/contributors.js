edb2b4fd93b0f2176702bd92720a0ec6088a8e09
Md Akram Kazmi <akramkazmi71@gmail.com>
2020-10-02 12:17:57 +0530

    files added

---
+++ src/components/contributors.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Contributor extends Component {
+    render() {
+        return(
+            <div>
+                <h1>Contributor</h1>
+            </div>
+        )
+    }
+}
+
+
+export default Contributor;
