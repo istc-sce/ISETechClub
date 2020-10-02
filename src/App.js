31db06f68e88c9489b09d7ce317de11829f12a59
Md Akram Kazmi <akramkazmi71@gmail.com>
2020-10-02 12:06:19 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import Switch from 'react-bootstrap/esm/Switch';
+import { Router, Route } from 'react-router-dom';
 import './App.css';
-import Main from './components/main.js';
 
 function App() {
   return (
-    <div className="App">
-      <Main />
-    </div>
+    <React.Fragment>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/event" component={Events} />
+          <Route exact path="/programming" component={Programming} />
+          <Route exact path="/result" component={Result} />
+          <Route exact path="/contributors" component={Contributors} />
+        </Switch>
+      </Router>
+    </React.Fragment>
   );
 }
 
