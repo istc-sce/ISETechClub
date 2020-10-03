a48f38934ee624a1aab5d751e13a4ee57d025459
niranjan hegde <hegdeniranjan144@gmail.com>
2020-10-03 13:36:27 +0530

    Updated contributors

--- src/components/contributors.js
+++ src/components/contributors.js
@@ -4,16 +4,18 @@ let mapDetail = (detail) => {
   return (
     <div className="contributor" key={detail.id}>
       <img src={detail.profile} alt="profile" />
-      <div className="about">
-        <div className="name">{detail.name}</div>
-        <div className="desc">{detail.description}</div>
-      </div>
-      <div className="contact">
-        {detail.contact.map((contact) => (
-          <a key="contact.id" href={contact.link}>
-            Profile
-          </a>
-        ))}
+      <div className="overlay">
+        <div className="about">
+          <div className="name">{detail.name}</div>
+          <div className="desc">{detail.description}</div>
+        </div>
+        <div className="contact">
+          {detail.contact.map((contact) => (
+            <a key="contact.id" href={contact.link}>
+              Profile
+            </a>
+          ))}
+        </div>
       </div>
     </div>
   );
