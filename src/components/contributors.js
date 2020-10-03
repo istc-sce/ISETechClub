ec9780d71689abefbed95550ab34b5d268a89431
Philippe BARRE <philippe.barre.57@gmail.com>
2020-10-03 18:16:47 +0200

    fix: change icon globe to desktop

--- src/components/contributors.js
+++ src/components/contributors.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGlobe, faPhone } from '@fortawesome/fontawesome-free-solid'
+// eslint-disable-next-line
+import { faDesktop, faPhone } from '@fortawesome/fontawesome-free-solid'
 
 let mapDetail = (detail) => {
   return (
