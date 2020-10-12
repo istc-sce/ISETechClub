1b886cc293c384a90dc87d354fe5ad45bb1c6e44
Md Akram Kazmi <akramkazmi71@gmail.com>
2020-10-12 13:31:22 +0530

    fork and note to home page

--- src/App.js
+++ src/App.js
@@ -15,12 +15,6 @@ import { Nav, Navbar } from "react-bootstrap";
 function App() {
   return (
     <div>
-      <a href="https://github.com/niranjanhegde144/ISETechClub" target="_blank" rel="noopener noreferrer" className='forkLink'>
-          <img
-            border='0' alt='Github' width='150' height='150'
-            src='https://github.blog/wp-content/uploads/2008/12/forkme_right_darkblue_121621.png?resize=149%2C149'
-          />
-        </a>
       <Navbar bg="light" collapseOnSelect={true} varient="light" expand="lg">
         <Navbar.Brand as={Link} to="/">
           <img
@@ -64,7 +58,6 @@ function App() {
         path="/team"
         render={(props) => <Contributors details={contributor_details} {...props} />}
       />
-      <p className="note">*NOTE: To contribute in this project, open in a desktop browser*</p>
     </div>
   );
 }
