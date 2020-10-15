be629746dcf4ed40151ecfaca4d5813c366f3614
Pranv <pranavbharadwaj142@gmail.com>
2020-10-15 12:38:26 +0530

    resolved all warnings

--- src/components/home.js
+++ src/components/home.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Container, Jumbotron } from 'react-bootstrap';
+import { Jumbotron } from 'react-bootstrap';
 import Teamimg from "./images/team.svg";
 import Vision from "./images/vision.svg";
 import Mission from "./images/mission.svg";
@@ -18,38 +18,38 @@ class Home extends Component {
                 </div>
                 <br />
                 <Jumbotron className="homeCard">
-                <section class="section" id="services">
-        <div class="paras">
+                <section className="section" id="services">
+        <div className="paras">
 
-            <p class="sectiontag text-big text1">About Us</p>
-            <p class="sectionsubtag text-small text2">ISTC OR Information Science Technical Club is a student run body founded by our alumni and students with the help of our HOD and few of our very experienced teachers in 2019.
+            <p className="sectiontag text-big text1">About Us</p>
+            <p className="sectionsubtag text-small text2">ISTC OR Information Science Technical Club is a student run body founded by our alumni and students with the help of our HOD and few of our very experienced teachers in 2019.
 In  today’s  rapidly  changing  environment,  programming  skills  are  essential  tools  that  can  be  utilized  and incorporated  into  various  fields  and  domains.  Hence,  it  becomes  absolutely  essential  to  equip  young minds  with  such  skills.  Coding  Club  aims  to  establish  a  coding  culture  on  campus,  reaching  every  student passionate  about  coding and reaching all of them.</p>
         </div>
-        <div class="thumb">
-            <img src={Teamimg} alt="idea" srcset="" class="imgfluid"/>
+        <div className="thumb">
+            <img src={Teamimg} alt="idea"  className="imgfluid"/>
         </div>
     </section>
     
-    <section class="section left">
-        <div class="paras">
+    <section className="section left">
+        <div className="paras">
 
-            <p class="sectiontag text-big text1">Mission</p>
-            <p class="sectionsubtag text-small text2">To give students the relevant skills, confidence and opportunities to change their coding environment and knowledge about tech. We believe in working and learning together. 
+            <p className="sectiontag text-big text1">Mission</p>
+            <p className="sectionsubtag text-small text2">To give students the relevant skills, confidence and opportunities to change their coding environment and knowledge about tech. We believe in working and learning together. 
 We want that through the events of near future we all learn and grow.</p>
         </div>
-        <div class="thumb">
-            <img src={Mission} alt="idea" srcset="" class="imgfluid"/>
+        <div className="thumb">
+            <img src={Mission} alt="idea"  className="imgfluid"/>
         </div>
     </section>
     
-    <section class="section">
-        <div class="paras">
+    <section className="section">
+        <div className="paras">
 
-            <p class="sectiontag text-big text1">Vision</p>
-            <p class="sectionsubtag text-small text2">We believe that realizing ideas of students will make a institute more powerful,we are trying to provide a platform for those hidden ideas within you. The main vision of ISTC  is to make a student succeed upto certain extent in realising their ideas. We also aims to strengthen the Coding culture, Entrepreneurship and team work among students.</p>
+            <p className="sectiontag text-big text1">Vision</p>
+            <p className="sectionsubtag text-small text2">We believe that realizing ideas of students will make a institute more powerful,we are trying to provide a platform for those hidden ideas within you. The main vision of ISTC  is to make a student succeed upto certain extent in realising their ideas. We also aims to strengthen the Coding culture, Entrepreneurship and team work among students.</p>
         </div>
-        <div class="thumb">
-            <img src={Vision} alt="idea" srcset="" class="imgfluid"/>
+        <div className="thumb">
+            <img src={Vision} alt="vision"  className="imgfluid"/>
         </div>
     </section>
                 </Jumbotron>
