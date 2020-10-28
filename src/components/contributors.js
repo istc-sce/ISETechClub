c5a2cd27b64222262b8128a2524a29ead924f459
Md Akram Kazmi <akramkazmi71@gmail.com>
2020-10-28 18:12:44 +0530

    separate css for each page

--- src/components/contributors.js
+++ src/components/contributors.js
@@ -2,6 +2,7 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDesktop } from "@fortawesome/free-solid-svg-icons";
 import { faGithub, faLinkedin, faTwitter,faGooglePlay } from "@fortawesome/free-brands-svg-icons";
+import "./contributors.css";
 
 const fontAwesomeStyle = {
   color: "white",
@@ -105,8 +106,6 @@ let mapDetail = (details) => {
 export default function Contributors(props) {
   const data = props.details;
   return (
-    <div className="teamBackground">
     <div className="profile">{data.map((detail) => mapDetail(detail))}</div>
-    </div>
   );
 }
--- src/components/programming.js
+++ src/components/programming.js
@@ -10,7 +10,7 @@ class Programming extends Component {
                         src='https://github.blog/wp-content/uploads/2008/12/forkme_right_darkblue_121621.png?resize=149%2C149'
                     />
                 </a>
-                <img className="bannerImageProgramming" src={process.env.PUBLIC_URL + "/banner.png"} alt="Under Construction" />
+                <img className="bannerImageProgramming" src={process.env.PUBLIC_URL + "/banner.png"} alt="Under Construction" width="100%" />
                 <p className="note">*NOTE: To contribute in this project, open in a desktop browser*</p>
             </div>
         )
